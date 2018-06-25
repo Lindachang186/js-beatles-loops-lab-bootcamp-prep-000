746db6f33c3e0826586dc28ad9c5d899f7fec0b0
Linda Chang <lindachang186@gmail.com>
2018-06-25 19:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (var i = 0; musicians.length > i; i++){
+  for (var i = 0; i < musicians.length; i++){
     array.push(`${musicians} plays ${instruments}`)
   }
   return array
